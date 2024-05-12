28dc41f6e5f4b9d26b86dd768ed981b77d3b44e6
Albinnich <albina_haji@hotmail.com>
2024-05-12 17:18:30 +0000

    Script handling cmds

---
+++ builtin_cmds_handler.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* handle_builtin_commands - Handles builtin commands
+* @cmd: The command to be handled
+* @args: The list of arguments
+* @status: The status code
+*
+* Return: 0 on success, 1 otherwise.
+*/
+
+int handle_builtin_commands(char *cmd, char **args, int status)
+{
+
+	if (strcmp(args[0], "exit") == 0)
+	{
+		free(cmd);
+		shell_exit(status);
+	}
+	else if (strcmp(args[0], "env") == 0)
+	{
+		print_env();
+		return (1);
+	}
+
+	return (0);
+}
