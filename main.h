91cad8dd08b4251bf91e90a74323d845b05f9b3b
Raid-dev <a_rauf2004@mail.ru>
2024-05-12 11:42:42 +0000

    Added new prototypes

--- main.h
+++ main.h
@@ -2,19 +2,28 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
-#include <stddef.h>
-#include <fcntl.h>
+
+#define MAX_CMD_LEN 256
+#define MAX_ARGV_LEN 64
+
 extern char **environ;
 
-int main(void);
-char *_getline(void);
-char **line_div(char *command, char **arr);
-void handle_env(char *line);
-void execute_command(char *command);
+int execute_cmd(char *cmd, char *argv[]);
+int line_div(char *line, char *args[]);
+char *get_file_loc(char *path, char *file_name);
+int slash_checker(const char *str);
+char *get_file_path(char *file_name);
+void handle_exit(char *cmd, char *args, int status);
+void shell_exit(int status);
+int handle_builtin_commands(char *cmd, char **args, int status);
+char *read_input(void);
+void handle_env(void);
+char *_getenv(const char *name);
 
 #endif
